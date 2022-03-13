49e9f65363b376f6d26504bbe5dbb49c85bdbf9a
Iheb Manai <iheb.manai@esprit.tn>
2022-03-13 23:47:29 +0100

    create PdfContentExtractor components : frontend

--- frontend/src/components/button-extract/ButtonExtract.tsx
+++ frontend/src/components/button-extract/ButtonExtract.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import { Button, styled } from '@mui/material';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Button, styled } from "@mui/material";
+import PropTypes from "prop-types";
 
-//ts-ignore
 const BootstrapButton = styled(Button)({
-  boxShadow: 'ButtonExtract',
-  textTransform: 'none',
+  boxShadow: "ButtonExtract",
+  textTransform: "none",
   fontSize: 16,
-  alignSelf: 'center',
-  width: '16rem',
-  border: '1px solid',
+  alignSelf: "center",
+  width: "16rem",
+  border: "1px solid",
   lineHeight: 1.5,
-  backgroundColor: '#0063cc',
-  borderColor: '#0063cc'
+  backgroundColor: "#0063cc",
+  borderColor: "#0063cc",
 });
 
 const ButtonExtract = ({ disabled, message, onClick }: any) => {
-  BootstrapButton;
-
   return (
     <>
-      <Button disabled={disabled} onClick={() => onClick()} variant="contained" size="large">
+      <Button
+        disabled={disabled}
+        onClick={() => onClick()}
+        variant="contained"
+        size="large"
+      >
         {message}
       </Button>
     </>
@@ -29,6 +31,6 @@ const ButtonExtract = ({ disabled, message, onClick }: any) => {
 ButtonExtract.propTypes = {
   onClick: PropTypes.func,
   disabled: PropTypes.bool,
-  message: PropTypes.string
+  message: PropTypes.string,
 };
 export default ButtonExtract;
