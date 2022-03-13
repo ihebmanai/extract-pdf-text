67531532f3e061a85e6cc63b47ff931aa59a3aa1
Iheb Manai <iheb.manai@esprit.tn>
2022-03-13 21:37:45 +0100

    install dependencies and cleanup server

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-const extractRouter = require('./routes/extract.route');
+const extractRouter = require("./routes/extract.route");
 
 const app = express();
 
-// view engine setup
-
-app.use(logger('dev'));
+app.use(logger("dev"));
+// allow cors policy
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/extract', extractRouter);
+app.use("/extract", extractRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
