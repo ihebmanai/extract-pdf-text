ac9ed6024682a6844f06db30aac81e95eaa9a7fa
Iheb Manai <iheb.manai@esprit.tn>
2022-03-13 22:15:29 +0100

    create header and footer components : frontend

---
+++ frontend/src/components/layout/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div className="footer">
+      <div>
+        <h2>Extract PDF File</h2>
+      </div>
+      <div className="footer__copyright">@ Copyright 2022</div>
+    </div>
+  );
+};
+
+export default MyComponent;
---
+++ frontend/src/components/layout/header/Header.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import ContractHeroLogo from '../../../assets/logos/contract-hero-logo.svg';
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
+            <img src={ContractHeroLogo} alt="contractHero logo" width={200} />
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Header;
