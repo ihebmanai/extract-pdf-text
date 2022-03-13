0c503082d9adddb7db2de0daacc52567f25001ae
Iheb Manai <iheb.manai@esprit.tn>
2022-03-13 21:51:01 +0100

    create extract route

--- routes/extract.route.js
+++ routes/extract.route.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { extractTextFromFile } = require("../contollers/extract.controller");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const upload = multer();
+
+/* POST :  extract text from files. */
+router.post("/", upload.single("pdfFile"), extractTextFromFile);
 
 module.exports = router;
