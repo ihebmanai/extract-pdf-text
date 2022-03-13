7ea5144a95a4eaafa183b7cd0d6811b96a3ec050
Iheb Manai <iheb.manai@esprit.tn>
2022-03-13 22:41:06 +0100

    create ButtonExtract components : frontend

---
+++ frontend/src/components/button-extract/ButtonExtract.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button, styled } from '@mui/material';
+import PropTypes from 'prop-types';
+
+//ts-ignore
+const BootstrapButton = styled(Button)({
+  boxShadow: 'ButtonExtract',
+  textTransform: 'none',
+  fontSize: 16,
+  alignSelf: 'center',
+  width: '16rem',
+  border: '1px solid',
+  lineHeight: 1.5,
+  backgroundColor: '#0063cc',
+  borderColor: '#0063cc'
+});
+
+const ButtonExtract = ({ disabled, message, onClick }: any) => {
+  BootstrapButton;
+
+  return (
+    <>
+      <Button disabled={disabled} onClick={() => onClick()} variant="contained" size="large">
+        {message}
+      </Button>
+    </>
+  );
+};
+ButtonExtract.propTypes = {
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  message: PropTypes.string
+};
+export default ButtonExtract;
