d258f57d3f5171c6396c021084d4550a258e3d05
Iheb Manai <iheb.manai@esprit.tn>
2022-03-13 22:40:31 +0100

    create FiledUploader components : frontend

---
+++ frontend/src/components/file-uploader/FileUploader.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import './FileUploader.css';
+import uploadImg from '../../assets/cloud-upload-regular-240.png';
+
+const FileUploader = (props) => {
+  const wrapperRef = useRef(null);
+
+  const onDragEnter = () => wrapperRef?.current?.classList.add('dragover');
+
+  const onDragLeave = () => wrapperRef?.current?.classList.remove('dragover');
+
+  const onDrop = () => wrapperRef?.current?.classList.remove('dragover');
+
+  const onFileDrop = (e: Event) => {
+    const newFile = e.target.files[0];
+    props.OnFileUploaded(newFile);
+  };
+
+  return (
+    <>
+      <div
+        ref={wrapperRef}
+        className="drop-file-input"
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}>
+        <div className="drop-file-input__label">
+          <img src={uploadImg} alt="" />
+          <p>Drag & Drop your files here</p>
+          <input type="file" value="" onChange={onFileDrop} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+FileUploader.propTypes = {
+  OnFileUploaded: PropTypes.func
+};
+
+export default FileUploader;
